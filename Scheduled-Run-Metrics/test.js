84448e9485f3aba64edfa2e22d713e98dd8e3991
nosvalds <niko@devinit.org>
2022-03-10 10:12:52 +0000

    try running full metrics

--- Scheduled-Run-Metrics/test.js
+++
@@ -1,3 +0,0 @@
-const config = require('../config/config');
-
-console.log(config);
--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { version } = require('../package.json');
 
 module.exports = {
-    APP_NAME: 'IATI App Name Here',
+    APP_NAME: 'IATI TCI Metrics',
     VERSION: version,
     NODE_ENV: process.env.NODE_ENV,
     APPINSIGHTS_INSTRUMENTATIONKEY: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
